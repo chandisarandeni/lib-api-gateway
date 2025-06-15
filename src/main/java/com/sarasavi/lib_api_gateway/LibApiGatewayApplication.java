5ad370514a80be3e2fb26b271987e4294f928cc0
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-15 18:05:00 +0530

    Add initial project structure with Maven config 🚀
    Add initial project structure with Maven configuration and application files

---
+++ src/main/java/com/sarasavi/lib_api_gateway/LibApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibApiGatewayApplication.class, args);
+	}
+
+}
