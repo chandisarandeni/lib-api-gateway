35d99061fcf318d27a0189566891b20078fab52f
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-15 19:50:56 +0530

    feat: add custom route locator for student API ✅

--- src/main/java/com/sarasavi/lib_api_gateway/LibApiGatewayApplication.java
+++ src/main/java/com/sarasavi/lib_api_gateway/LibApiGatewayApplication.java
@@ -2,12 +2,27 @@ package com.sarasavi.lib_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(LibApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/students/**")
+                        .filters(f -> f
+                                .addResponseHeader("X-Response-Header", "LibApiGateway")
+                        )
+                        .uri("http://localhost:8081")
+                )
+                .build();
+    }
 }
