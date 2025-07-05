4c537bb450c4fe9bf1a3a4ac656c517389161c79
chandisarandeni <chandisarandeni2003@gmail.com>
2025-07-05 16:44:07 +0530

    feat: implement ratings route ✅

--- src/main/java/com/sarasavi/lib_api_gateway/LibApiGatewayApplication.java
+++ src/main/java/com/sarasavi/lib_api_gateway/LibApiGatewayApplication.java
@@ -25,6 +25,14 @@ public class LibApiGatewayApplication {
                         .uri("http://localhost:8083")
                 )
 
+                // Ratings Service Route
+                .route(r -> r.path("/api/v1/ratings/**")
+                        .filters(f -> f
+                                .addResponseHeader("X-Response-Header", "LibApiGateway")
+                        )
+                        .uri("http://localhost:8086")
+                )
+
                 // Member Service Route
                 .route(r -> r.path("/api/v1/members/**")
                         .filters(f -> f
